08145aea1676cae03a16755f9284424f41776b7d
pkzrnvch <pkazarnovich@gmail.com>
2022-04-30 00:38:22 +0500

    Added pagination, displayed book genres

--- render_website.py
+++ render_website.py
@@ -1,5 +1,7 @@
+import math
 import os
 import json
+from math import ceil
 
 from livereload import Server
 from more_itertools import chunked
@@ -11,7 +13,8 @@ def rebuild():
     books_per_page = 20
     with open('book_descriptions.json') as json_file:
         books = json.load(json_file)
-    book_chunks = list(chunked(books, books_per_page))
+    book_chunks = chunked(books, books_per_page)
+    total_pages = ceil(len(books) / books_per_page)
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
@@ -20,7 +23,9 @@ def rebuild():
     for page, book_chunk in enumerate(book_chunks, start=1):
         book_pairs = list(chunked(book_chunk, 2))
         rendered_page = template.render(
-            book_pairs=book_pairs
+            book_pairs=book_pairs,
+            total_pages=total_pages,
+            current_page_number=page,
         )
         with open(f'./pages/index{page}.html', 'w', encoding="utf8") as file:
             file.write(rendered_page)
